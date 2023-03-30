08cb88f6edd81177ee1d3218353dd9c747273659
ErikHenrysson <henryssonerik@gmail.com>
2023-03-30 12:04:49 +0200

    placeholder for draw method

--- presenter.py
+++ presenter.py
@@ -1,20 +1,26 @@
 #Tanken med presenter är att den får färdig information som den kan presentera med förslagsvis matplotlib
 from receiver import *
 from mqtt_client import *
+import paho.mqtt.client as mqtt
+
 class presenter:
     def __init__(self, mqtt_client):
         self.mqtt_client = mqtt_client
 
 
     def show(self):
+        self.mqtt_client.my_loop()
         if self.mqtt_client.new_available_message():
             #if the client has new message, read the new message
-            message = self.mqtt_client.read_message()
-            self.draw(message)
+            self.message = self.mqtt_client.read_message()
+            if self.message != None:
+                self.draw(self.message)
 
     #extract useful information and draw the information with matplotlib?
     def draw(self, message):
-        pass
+        #Placeholder, just prints the message instead
+        print(message)
+
 
     #Extracts the desired parameters from the mqtt json message
     def extract_json(self, desired_params):
