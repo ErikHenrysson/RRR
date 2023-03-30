08c63bee2e5244f480ceca5c084cd5b3d1a75703
ErikHenrysson <henryssonerik@gmail.com>
2023-03-30 16:27:45 +0200

    added get methods and target id

--- my_target.py
+++ my_target.py
@@ -1,7 +1,22 @@
 class my_target:
-    def __init__(self, x:float, y:float, z:float, vel:float):
+    def __init__(self, x:float, y:float, z:float, vel:float, id:int):
         self.x = x
         self.y = y
         self.z = z
         self.vel = vel
-        
+        self.id = id
+        #self.print_target()
+
+    def print_target(self):
+        print("Target", self.id,"at (x, y, z) = (", self.x, ", ", self.y, ", ", self.z, ")\nTarget speed: ", self.vel, " m/s")
+
+    def get_x(self) -> float:
+        return self.x
+    def get_y(self) -> float:
+        return self.y
+    def get_z(self) -> float:
+        return self.z
+    def get_vel(self) -> float:
+        return self.vel
+    def get_id(self) -> int:
+        return self.id
