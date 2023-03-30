42cbdabe509f98e60b6f1d13948a0184b885d23d
ErikHenrysson <henryssonerik@gmail.com>
2023-03-30 12:04:06 +0200

    slimmed down main

--- main.py
+++ main.py
@@ -3,15 +3,7 @@ from fsm import *
 from controller import *
 from mqtt_client import *
 from presenter import *
+from vehicle import *
 #for debugging
 
-idle_state = idle()
-mobile_state = mobile()
-recon_state = recon()
-
-state_list = [idle_state, mobile_state, recon_state]
-
-my_fsm = fsm(state_list=state_list)
-my_controller = controller(my_fsm, "ps4")
-my_mqtt_client = mqtt_client("192.168.4.1", 1883, "eazense/eazense_38FDFEB810B6/out")
-my_presenter = presenter(my_mqtt_client)
+my_vehicle = vehicle()
