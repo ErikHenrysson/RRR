083a18a0441af09b449bcdb59471eb301eaa5961
ErikHenrysson <henryssonerik@gmail.com>
2023-04-05 14:43:08 +0200

    added created_time

--- my_target.py
+++ my_target.py
@@ -1,14 +1,15 @@
 class my_target:
-    def __init__(self, x:float, y:float, z:float, vel:float, id:int):
+    def __init__(self, x:float, y:float, z:float, vel:float, id:int, created_time:int):
         self.x = x
         self.y = y
         self.z = z
         self.vel = vel
         self.id = id
+        self.created_time = created_time
         #self.print_target()
 
     def print_target(self):
-        print("Target", self.id,"at (x, y, z) = (", self.x, ", ", self.y, ", ", self.z, ")\nTarget speed: ", self.vel, " m/s")
+        print("Target:", self.id," created at time:",self.created_time, "at (x, y, z) = (", self.x, ", ", self.y, ", ", self.z, ")\nTarget speed: ", self.vel, " m/s")
 
     def get_x(self) -> float:
         return self.x
@@ -20,6 +21,8 @@ class my_target:
         return self.vel
     def get_id(self) -> int:
         return self.id
+    def get_created_time(self):
+        return self.created_time
     def set_x(self, x:float):
         self.x = x
     def set_x(self, y:float):
