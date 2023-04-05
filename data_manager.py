c34c8455e83b9527192e0cb4247e8d8adda5ce91
ErikHenrysson <henryssonerik@gmail.com>
2023-04-05 14:16:29 +0200

    changed name of presenter to data_manager

--- data_manager.py
+++ data_manager.py
@@ -8,7 +8,7 @@ from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 
-class presenter:
+class data_manager:
     def __init__(self, mqtt_client: mqtt_client):
         self.mqtt_client = mqtt_client
         self.targets:list[my_target] = []
@@ -22,24 +22,16 @@ class presenter:
         if self.mqtt_client.new_available_message():
             #if the client has new message, read the new message
             self.message = self.mqtt_client.read_message()
+            #print(self.message)
             #Extract the targets and remove duplicates
             self.extract_json(self.message)
-            with open('radarData.txt', 'a') as f:
+            with open('radarData.txt', 'w') as f:
                 for target in self.targets:
                     lines = [str(target.get_x()), ',',str(target.get_y()),',', str(target.get_z()),',',str(target.get_vel()),'\n']
                     f.writelines(lines)
 
-    #extract useful information and draw the information with matplotlib?
-    #TODO present the target using numpy and matplotlib
-    #TODO Maybe create the plot once in the constructor and just clear it before creating it again?
-    def draw(self, message) -> str:
-    
-        
-        return message
-
-
     #Extracts the desired parameters from the mqtt json message
-    #Creates a list of new targets, which can be compared to the old ones
+    #Creates new targets that can be comprade with old ones to see if they are new or not
     def extract_json(self, message) -> list:
         #for testing
         json_message = json.loads(message)
@@ -47,12 +39,12 @@ class presenter:
         num_targets = detected_targets['noOfPerson']
         if num_targets != 0:
             for target in detected_targets['persons']:
-                target = my_target(float(target['x']),float(target['x']),float(target['z']),float(target['activity']), self.id)
+                target = my_target(float(target['x']),float(target['y']),float(target['z']),float(target['activity']), self.id)
                 self.id += 1
                 is_new_target = self.compare_new_target(target)
                 if not is_new_target:
                     self.id -=1
-
+    
     # Maybe its better to compare a single target each time it is created, easier to keep track of target ids that way.
     def compare_new_target(self, new_target:my_target) -> bool:
         if bool(self.targets):
@@ -66,13 +58,14 @@ class presenter:
                 z2:float = new_target.get_z()
                 #pythagoras to get distance between two points in 3d room
                 distance = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2))
-                #distance = sqrt((target.get_x-new_target.get_x)*(target.get_x-new_target.get_x) + \
-                #    (target.get_y-new_target.get_y)*(target.get_y - new_target.get_y) + \
-                #    (target.get_z-new_target.get_z)*(target.get_z - new_target.get_z))
-                #if its lower than the threshold, the new_target is not considered "a new target" dont put it into the target list
-                #TODO if its lower than the threshold, replace the old target with the new location of the same target
                 print(distance)
+                #if it is the same target, update the old targets information
                 if distance <= self.threshold:
+                    target.set_x(new_target.get_x)
+                    target.set_y(new_target.get_y)
+                    target.set_z(new_target.get_z)
+                    target.set_vel(new_target.get_vel)
+                    
                     return False
                 else:
                     #if it is a new target, return true and add it to the new target list 
@@ -83,9 +76,12 @@ class presenter:
             self.targets.append(new_target)
             return True
 
+    #should be called regurarely
+    def loop_mqtt(self):
+        self.mqtt_client.my_loop()
 
 
-
+    '''
     #for each pair of targets, check if they are close enough to each other that they probably are the same target
     #should be able to set a threshhold for this value
     #TODO decide how to compare the different targets to determine if they are the same or not and figure out if this one is better than the single one
@@ -112,12 +108,9 @@ class presenter:
             while(bool(self.new_targets)):
                 self.targets.append(self.new_targets.pop())    
 
+        '''
 
 
-
-    #should be called regurarely
-    def loop_mqtt(self):
-        self.mqtt_client.my_loop()
 
 #mymqttclient = MqttClient("test.mosquitto.org", 1883, "eazense/eazense_38FDFEB810B6/out")
 #print("Made iot past the creation of the mqttclient")
