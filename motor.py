a243648a17794d9ffb98ff38424c03f02f967d54
ErikHenrysson <104365671+ErikHenrysson@users.noreply.github.com>
2023-05-08 12:11:57 +0200

    Delete motor.py

--- motor.py
+++
@@ -1,2 +0,0 @@
-#Tanken är att skapa en klass som heter motor, förslagsvis skapas fyra instanser av denna klass för alla hjul på roboten
-#Dessa används sedan i vehicle.py för att styra hela roboten
