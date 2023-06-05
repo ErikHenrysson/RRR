9ff1bef91d6000763a7929266740036791cb585e
ErikHenrysson <104365671+ErikHenrysson@users.noreply.github.com>
2023-06-05 17:23:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from gui import *
 from multiprocessing import Process
 from vehicleCar import *
 
-#for debugging
 
 my_gui = gui()
-#run = True
-#while run:
-#    gui.update()
-my_vehicle = vehicle("192.168.4.1", 1883, "eazense/eazense_38FDFEB810B6/out", my_gui)
-p1 = Process(target=run_controller(my_vehicle))
+
+my_vehicle = vehicle("192.168.4.1", 1883, "eazense/eazense_38FDFEB810B5/out", my_gui)
+my_vehicle.change_state("recon")
+#Skapa kontrollen
+my_controller = MyController(my_vehicle, interface="/dev/input/js0", connecting_using_ds4drv=False)
+#gör listen() som process istället
+print("efter mycontroller")
+p1 = Process(target = my_controller.our_listen, args=(my_vehicle,))
+#p1 = Process(target=run_controller,args= (my_vehicle,))
+print("före start1 i main")
 p1.start()
+print("efter start1 i main")
 my_vehicle.run()
+#p2 = Process(target=my_vehicle.run)
+#p2.start()
+print("efter start2 i main")
